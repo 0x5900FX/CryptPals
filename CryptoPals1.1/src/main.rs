20756f1fecc62071ffee6a498524487c958a13f9
CatDuckNotFound <404niraj@gmail.com>
2024-06-29 21:48:40 +0545

    Added User input & Changed into one merged two function

--- CryptoPals1.1/src/main.rs
+++ CryptoPals1.1/src/main.rs
@@ -1,29 +1,29 @@
+use std::io;
 //dependencies
 use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
-use hex::FromHexError;
 
 /// A custom base64 engine to encode and decode
 const CUSTOM_ENGINE: engine::GeneralPurpose =
     engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
-//Convert hexadecimal to plain_text
-fn hex_to_plain(input: &str) -> Result<Vec<u8>, FromHexError> {
-    hex::decode(input)
+//Convert hexadecimal to base64
+
+fn hex_to_base64(input: String) -> String {
+    println!("Your user input is {}", input);
+
+    let decoded_hex =     hex::decode(input).unwrap();
+    CUSTOM_ENGINE.encode(decoded_hex)
 }
-//convert plain_to_base64
-fn plain_to_base64(input: Vec<u8>) -> String {
-    CUSTOM_ENGINE.encode(input)
-}
+
 
 fn main() {
-    let inp = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-let output = format!("{}",plain_to_base64(hex_to_plain(inp).unwrap()));
-println!("{}",output);
 
+    println!("Enter your string \n You're converting from hex to base64 \n");
+    let mut user_inp = String::new();
+    io::stdin().read_line(&mut user_inp).expect("Failed to read line");
 
-
-
-
-
+    let output = hex_to_base64(user_inp.trim().to_string());  //removes newlines and extra spaces
+    let final_out  = format!("{}",output);
+    println!("Your HEx to Base64 is  \n{}",final_out);
 
 }
